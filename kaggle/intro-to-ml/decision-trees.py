ffd5750112cbf22086fe1f65991e3071e565e5f1
mallett002 <mallett002@gmail.com>
2025-01-09 19:26:55 -0600

    notes on decision tree

--- kaggle/intro-to-ml/decision-trees.py
+++ kaggle/intro-to-ml/decision-trees.py
@@ -11,6 +11,23 @@ home_data = pd.read_csv(iowa_file_path)
     # X is features
     # y is targets
 
+
+# ---------------------------------------
+# How this works
+# Training and Validation
+# Split the data into training data and validation data
+# Each has features (X) and targets (y)
+
+# Train them model
+
+# Make predictions with validation data 
+# Calculate MAE - See how far off from actual targets
+
+# Find sweet spot with underfitting and overfitting
+# ---------------------------------------
+
+
+
 # -------------------------------------------------------------------------------------------------------------
 # 1. Find prediction target (thing you want the AI to figure out)
 home_data.columns # search through columns, looking for one related to home price
@@ -44,43 +61,43 @@ train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 1)
 # -------------------------------------------------------------------------------------------------------------
 # 4. Create the model
 # For model reproducibility, set a numeric value for random_state when specifying the model
-# iowa_model = DecisionTreeRegressor(random_state=1)
+iowa_model = DecisionTreeRegressor(random_state=1)
 
 
 
 # -------------------------------------------------------------------------------------------------------------
 # 5. Fit (Train) the model
-# iowa_model.fit(train_X, train_y) # use training data (features X, and target y)
+iowa_model.fit(train_X, train_y) # use training data (features X, and target y)
 
 
 
 # -------------------------------------------------------------------------------------------------------------
 # 6. Make predictions 
 # Based on validation features, make predictions on what target (val_y) will be 
-# val_predictions = iowa_model.predict(val_X)
+val_predictions = iowa_model.predict(val_X)
 
 # print the top few validation predictions (targets that the model created)
-# print(val_predictions[:5])
+print(val_predictions[:5])
 
 # print the top few actual prices (expected prices) from validation data
-# print(val_y.head())
+print(val_y.head())
 
-# print("First in-sample predictions:", iowa_model.predict(X.head()))
-# print("Actual target values for those homes:", y.head().tolist())
+print("First in-sample predictions:", iowa_model.predict(X.head()))
+print("Actual target values for those homes:", y.head().tolist())
 
 
 
 # -------------------------------------------------------------------------------------------------------------
 # 7. Calculate Mean Absolute Error
 # Determines by how much are we off with the actual target
-# val_mae = mean_absolute_error(val_y, val_predictions) # --> 29652.931506849316
+val_mae = mean_absolute_error(val_y, val_predictions) # --> 29652.931506849316
 
 # Find sweet spot between underfitting and overfitting by comparing max_leaf_nodes' MAE
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
     model.fit(train_X, train_y)
-    preds_val = model.predict(val_X)
-    mae = mean_absolute_error(val_y, preds_val)
+    val_predictions = model.predict(val_X)
+    mae = mean_absolute_error(val_y, val_predictions)
     return(mae)
 
 # search throgh max_leaf_nodes to find the sweet spot:
@@ -93,5 +110,11 @@ final_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=0)
 
 # fit the final model and uncomment the next two lines
 final_model.fit(X, y)
+
+
+
+
+
+
 
 
