52ae4a12bdff5b5471448cb8a2685012b891ac28
mallett002 <mallett002@gmail.com>
2025-01-24 06:18:50 -0600

    categorical vars

--- kaggle/04_intermediate_ml/02_categorical_vars.py
+++ kaggle/04_intermediate_ml/02_categorical_vars.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 
 # How to deal with categorical vars??
 # 3 approaches:
@@ -55,10 +58,8 @@ X_valid = X_valid_full[feature_cols].copy()
 
 
 # 2. Obtain a list of all of the categorical variables in the training data.
-
 # create series of dtypes and if they are strings (index is colName)
 s = (X_train.dtypes == 'object')
-
 object_cols = list(s[s].index) # --> ['Type', 'Method', 'Regionname']
 
 
@@ -77,3 +78,48 @@ drop_y_train = y_train.select_dtypes(exclude=['object'])
 score_drop_vars = score_dataset(drop_X_train, drop_y_train, y_train, y_valid)
 print(score_drop_vars)
 
+
+
+
+# 4. Score from approach 2 (ordinal encoding)
+# Make copy to avoid changing original data
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+# Apply ordinal encoder to each column with categorical data
+ordinal_encoder = OrdinalEncoder()
+label_X_train[object_cols] = ordinal_encoder.fit_transform(X_train[object_cols])
+label_X_valid[object_cols] = ordinal_encoder.transform(X_valid[object_cols])
+
+print("MAE from Approach 2 (Ordinal Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+
+
+
+
+# 5. Score from Approach 3 (One-Hot Encoding)
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+# one-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add on-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+# Ensure all column names have type string 
+OH_X_train.columns = OH_X_train.columns.astype(str)
+OH_X_valid.columns = OH_X_valid.columns.astype(str)
+
+print("MAE from Approach 3 (One-Hot Encoding):") 
+print(score_dataset(OH_X_train, OH_X_valid, y_train, y_valid))
+
+# Typically One-Hot encoding performes the best
