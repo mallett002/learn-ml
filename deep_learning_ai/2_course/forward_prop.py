a7e36567b2f8e6948165b6d02b84876b86f6f699
mallett002 <mallett002@gmail.com>
2025-03-09 07:53:55 -0500

    more common forward prop

--- deep_learning_ai/2_course/forward_prop.py
+++ deep_learning_ai/2_course/forward_prop.py
@@ -45,3 +45,54 @@ b2_1 = np.array([3])
 z2_1 = np.dot(w2_1, a1) + b2_1
 a2 = sigmoid(z2_1)
 
+
+
+#######################################
+### More common/programatic approach ##
+#######################################
+
+# define a dense func:
+def dense(a_in, W, b):
+    units = W.shape[1] # Get the amount of units (3 in this ex)
+    a_out = np.zeros(units) # start as the amount of units (the amount of outputs)
+
+    for j in range(units): # 0, 1, 2
+        w = W[:, j] # pull out "j"th col in the matrix (w1_1, then w1_2, etc.)
+        z = np.dot(w, a_in) + b[j]
+        a_out[j] = sigmoid(z)
+    
+    return a_out
+
+# How you would define sequential to create the Nueral network:
+# def sequential(X):
+#     a1 = dense(X, W1, b1) # note: capitals refering to matrixes in linear algebra
+#     a2 = dense(a1, W2, b2)
+#     a3 = dense(a2, W3, b3)
+#     a4 = dense(a3, W4, b4)
+#     f_x = a4
+#     return f_x
+
+
+# W for first layer:
+# w1_1 = [1,2]
+# w1_2 = [-3,4]
+# w1_3 = [5,-6]
+
+# 2 x 3 array
+# 2 rows: number of input features
+# 3 cols: number of nuerons (units)
+W = np.array([
+    [1, -3, 5], # feature set 1
+    [2, 4, -6]  # feature set 2
+]) # ^  ^   ^  
+#   n1  n2  n3
+
+
+# b for first layer:
+# b1_1 = -1
+# b1_2 = 1
+# b1_3 = 2
+b = np.array([-1, 1, 2])
+
+# X or a[0]
+a_in = np.array([-2, 4])
