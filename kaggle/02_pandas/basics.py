d0280ffe50325e04f0a02ec7505117e6df977d7e
mallett002 <mallett002@gmail.com>
2025-12-22 06:43:56 -0600

    saving after long time not using/seeing

--- kaggle/02_pandas/basics.py
+++ kaggle/02_pandas/basics.py
@@ -7,8 +7,6 @@ import pandas as pd
 # print(random_df_data.head())
 
 
-
-
 # Making a data frame from sratch with index labels:
 fruit_sales = pd.DataFrame({
     'Apples': [35, 41, 19, 10],
@@ -16,21 +14,18 @@ fruit_sales = pd.DataFrame({
     'Kiwis': [92, 48, 19, 9],
 }, index=(['2017 Sales', '2018 Sales', '2019 Sales', '2020 Sales']))
 # Makes this vv
-#                 Apples	Bananas     Kiwis   
+#                 Apples	Bananas     Kiwis
 # 2017 Sales	  35	    21          92
 # 2018 Sales	  41	    34          48
 # 2019 Sales	  19        31          19
 # 2020 Sales	  10         3           9
 
 
-
-
-
 # Make a series with index labels and a name:
 ingredients = pd.Series(
     ['4 cups', '1 cup', '2 large', '1 can'],
-     index=['Flour', 'Milk', 'Eggs', 'Spam'],
-     name='Dinner')
+    index=['Flour', 'Milk', 'Eggs', 'Spam'],
+    name='Dinner')
 # Makes this vv
 
 # Flour     4 cups
@@ -40,11 +35,10 @@ ingredients = pd.Series(
 # Name: Dinner, dtype: object
 
 
-
-
 # Write a csv to file from a data frame:
-animals = pd.DataFrame({'Cows': [12, 20], 'Goats': [22, 19]}, index=['Year 1', 'Year 2'])
-animals.to_csv('cows_and_goats.csv') # vv
+animals = pd.DataFrame({'Cows': [12, 20], 'Goats': [22, 19]}, index=[
+                       'Year 1', 'Year 2'])
+animals.to_csv('cows_and_goats.csv')  # vv
 # ,Cows,Goats
 # Year 1,12,22
 # Year 2,20,19
@@ -68,8 +62,8 @@ fruit_sales.iloc[1:3, 0]
 fruit_sales.iloc[[0, 1, 2], 0]
 
 # Selecting from the end
-fruit_sales.iloc[-3, :] # Select 3rd to last row, all columns
-fruit_sales.iloc[-3:] # selects multiple rows, end to 3rd to last, all columns
+fruit_sales.iloc[-3, :]  # Select 3rd to last row, all columns
+fruit_sales.iloc[-3:]  # selects multiple rows, end to 3rd to last, all columns
 
 
 # Test questions
@@ -90,8 +84,6 @@ fruit_sales.iloc[0:3, 1]
 # print(fruit_sales.iloc[:-1, 1]) # what AI says (all but last row, 2nd col)
 
 
-
-
 # Label based selection with loc[row(start:stop), col(start:stop)]  start(inclusive):stop(inclusive)
 # Get first entry in Apples:
 fruit_sales.loc['2017 Sales', 'Apples']
@@ -103,19 +95,15 @@ fruit_sales.loc[:, ['Apples', 'Kiwis']]
 fruit_sales.set_index('Apples')
 
 
-
-
-
-
 # Conditional selection:
 reviews = pd.read_csv('./wine_reviews_sample.csv')
 # reviews.country == 'Italy' # produces series of True False bools
 
 # get just the reviews that are from Italy
-reviews.loc[reviews.country == 'Italy'] 
+reviews.loc[reviews.country == 'Italy']
 
 # get Italian reviews over 90
-reviews.loc[(reviews.country == 'Italy') & (reviews.points >= 90)] 
+reviews.loc[(reviews.country == 'Italy') & (reviews.points >= 90)]
 reviews.loc[reviews.country.isin(['Italy', 'France'])]
 
 # get Italian reviews or those rated over 90
@@ -125,29 +113,23 @@ reviews.loc[(reviews.country == 'Italy') | (reviews.points >= 90)]
 reviews.loc[reviews.price.notnull()]
 
 
-
-
-
 # Assigning data
 reviews['critic'] = 'Billy'
 reviews.critic
 
-#range(start, stop, step)
-list(range(5, 0, -1)) # start at 5, stop at 0 exclusive, go back 1 each iteration
+# range(start, stop, step)
+list(range(5, 0, -1))  # start at 5, stop at 0 exclusive, go back 1 each iteration
 
 # reviews['index_backwards'] = range(len(reviews), 0, -1)
 # reviews['index_backwards']
 
 
-
-
-
 ##### Summary Functions ####
-reviews.describe() # see high level aggregate info
-reviews.taster_name.describe() # see aggregate info on taster_name (string value)
-reviews.points.mean() # see average of points allotted
-reviews.taster_name.unique() # see all the taster names
-reviews.taster_name.value_counts() # See tasters and how often they occur in data
+reviews.describe()  # see high level aggregate info
+reviews.taster_name.describe()  # see aggregate info on taster_name (string value)
+reviews.points.mean()  # see average of points allotted
+reviews.taster_name.unique()  # see all the taster names
+reviews.taster_name.value_counts()  # See tasters and how often they occur in data
 
 
 # map higher level function
@@ -160,15 +142,15 @@ reviews.points.map(lambda p: p - review_points_mean)
 ##########################################
 # apply - Similar to map, but transforms whole dataframe by calling custom dataframe on each row
 # axis:
-    # 0 or "index" --> (vertical) Doing something to each column (default)
-    # 1 or "columns" (horizontal) Doing something to each row
+# 0 or "index" --> (vertical) Doing something to each column (default)
+# 1 or "columns" (horizontal) Doing something to each row
 
 def remean_points(row):
     row.points = row.points - review_points_mean
     return row
 
-reviews.apply(remean_points, axis='columns') 
 
+reviews.apply(remean_points, axis='columns')
 
 
 # How axis works in apply: ---------------------------------------------------------------
@@ -183,20 +165,28 @@ treats = pd.DataFrame({
 # print('\n\n')
 
 # 0 --> columns (vertical) Doing something to each column (default)
+
+
 def sum_cols(df):
     return df.sum()
 
-treats_sold = treats.apply(lambda df: df.sum()) # --> sums the data for each column (0 or "index")
+
+# --> sums the data for each column (0 or "index")
+treats_sold = treats.apply(lambda df: df.sum())
 
 # print("Treats sold by treat:")
 # print(treats_sold)
 # print('\n\n')
 
 # 1 --> index  (horizontal) Doing something to each row
+
+
 def sum_rows(row):
     return row.sum()
 
-treats_sold_by_row = treats.apply(sum_rows, axis=1) # --> sums the data for each row (1 or "columns")
+
+# --> sums the data for each row (1 or "columns")
+treats_sold_by_row = treats.apply(sum_rows, axis=1)
 
 # print("Treats sold by row:")
 # print(treats_sold_by_row)
@@ -212,17 +202,21 @@ example_reviews = pd.DataFrame({
     'other_column': ['A', 'B', 'C']
 })
 
- # Calculate the mean of 'points'
+# Calculate the mean of 'points'
 review_points_mean = example_reviews['points'].mean()
 # print(review_points_mean)
 
- # Define the function to remean points
+# Define the function to remean points
+
+
 def remean_points(row):
-   row['points'] = row['points'] - review_points_mean
-   return row
+    row['points'] = row['points'] - review_points_mean
+    return row
+
 
 # Apply the function to each row
-example_reviews = example_reviews.apply(remean_points, axis=1) # columns, so do something to each row
+# columns, so do something to each row
+example_reviews = example_reviews.apply(remean_points, axis=1)
 
 # print(example_reviews['points'])
 
@@ -238,7 +232,8 @@ reviews.country + '-' + reviews.region_1
 
 
 # Get wine with highest points-to-price ratio
-bargain_idx = (reviews.points / reviews.price).idxmax() # gets the row label of the highest value
+# gets the row label of the highest value
+bargain_idx = (reviews.points / reviews.price).idxmax()
 bargain_wine = reviews.loc[bargain_idx, 'title']
 
 
@@ -258,15 +253,12 @@ def apply_stars(row):
 
     return 1
 
+
 # The answer:
 reviews_with_stars = reviews.apply(apply_stars, axis='columns')
 
 # Can add stars on the reviews DF like this now:
 reviews['stars'] = reviews_with_stars
-
-
-
-
 
 
 ##### Grouping and Sorting ####
@@ -279,20 +271,22 @@ reviews.groupby('points').points.count()
 
 reviews.groupby('points').price.min()
 # groups points together points --> price
-# 87 --> [15.99, 39.99, 29.00] 
+# 87 --> [15.99, 39.99, 29.00]
 # 91 --> [21.99, 93.99]
 # 59 --> ...
 # Then, selects the min for each one
 
 
 # Select name of first wine reviewed for each winery in dataset
-first_wines = reviews.groupby('winery').apply(lambda df: df.title.iloc[0], include_groups=False)
+first_wines = reviews.groupby('winery').apply(
+    lambda df: df.title.iloc[0], include_groups=False)
 # ^^ include_groups=False removes 'winery from the df
 
 # Pick best wine by country and province
-reviews.loc[0] # --> first row
-reviews.points.idxmax() # --> gets the index of the row with most points
-best_wines_by_country_prov = reviews.groupby(['country', 'province']).apply(lambda df: df.loc[df.points.idxmax()], include_groups=False)
+reviews.loc[0]  # --> first row
+reviews.points.idxmax()  # --> gets the index of the row with most points
+best_wines_by_country_prov = reviews.groupby(['country', 'province']).apply(
+    lambda df: df.loc[df.points.idxmax()], include_groups=False)
 
 # agg: run several summary functions on DF simultaneously
 statistical_summary = reviews.groupby('country').price.agg([len, 'min', 'max'])
@@ -300,16 +294,20 @@ statistical_summary = reviews.groupby('country').price.agg([len, 'min', 'max'])
 
 # Multi-indexes
 # has 2 row labels:
-countries_reviewed = reviews.groupby(['country', 'province']).description.agg([len])
+countries_reviewed = reviews.groupby(
+    ['country', 'province']).description.agg([len])
 # convert back into regular index:
 # countries_reviewed.reset_index()
 
 # Sorting
 # countries_reviewed = countries_reviewed.reset_index()
-countries_reviewed = countries_reviewed.sort_values(by='len') # sort results asc
-countries_reviewed = countries_reviewed.sort_values(by='len', ascending=False) # sort results desc 
-countries_reviewed.sort_index() # sort by index
-countries_reviewed.sort_values(by=['country', 'len']) # sort by more than 1 col at a time
+countries_reviewed = countries_reviewed.sort_values(
+    by='len')  # sort results asc
+countries_reviewed = countries_reviewed.sort_values(
+    by='len', ascending=False)  # sort results desc
+countries_reviewed.sort_index()  # sort by index
+# sort by more than 1 col at a time
+countries_reviewed.sort_values(by=['country', 'len'])
 
 
 # practice for grouping and sorting
@@ -326,7 +324,8 @@ price_extremes = reviews.groupby('variety').price.agg(['min', 'max'])
 
 
 # Most expensive wine varieties
-sorted_varieties = price_extremes.sort_values(by=['min', 'max'], ascending=False)
+sorted_varieties = price_extremes.sort_values(
+    by=['min', 'max'], ascending=False)
 
 # Series. index is reviewers. values is avg score by that reviewer
 reviewer_mean_ratings = reviews.groupby('taster_name')['points'].mean()
@@ -334,24 +333,17 @@ reviewer_mean_ratings = reviews.groupby('taster_name')['points'].mean()
 # What combination of countries and varieties are most common?
 # Series index is MultiIndex of {country, variety} pairs.
 # Sort vales in Series in desc based on wine count
-country_variety_counts = reviews.groupby(['country', 'variety']).size().sort_values(ascending=False) # sort results desc 
+country_variety_counts = reviews.groupby(['country', 'variety']).size(
+).sort_values(ascending=False)  # sort results desc
 # reviews.groupby(['country', 'variety']) --> Group them into pairs of country,variety
 # Get the amount of occurances of this combination --> .size()
 # sort them in descending order --> .sort_values(ascending=False)
 
 
-
-
-
-
-
-
-
-
 ##### Data Types and Missing Values #####
 # See types:
-reviews.price.dtype # --> on column
-reviews.dtypes # --> on all df columns
+reviews.price.dtype  # --> on column
+reviews.dtypes  # --> on all df columns
 
 # Change dtypes
 reviews.points.astype('float32')
@@ -361,12 +353,14 @@ reviews.price.dtype
 point_strings = reviews.points.astype('str')
 
 # transform whole df
-memory_effecient_reviews = reviews.astype({'points': 'int32', 'stars': 'int32', 'price': 'float32'})
+memory_effecient_reviews = reviews.astype(
+    {'points': 'int32', 'stars': 'int32', 'price': 'float32'})
 
 
 # Missing data
-pd.isnull(reviews.country) # --> Series: indexes where reviews.country is null
-reviews[pd.isnull(reviews.country)] # --> gets new df with rows that have missing country
+pd.isnull(reviews.country)  # --> Series: indexes where reviews.country is null
+# --> gets new df with rows that have missing country
+reviews[pd.isnull(reviews.country)]
 
 # find amount of rows with null country
 no_country = len(reviews[reviews.country.isnull()])
@@ -378,7 +372,8 @@ no_country = pd.isnull(reviews.country).sum()
 reviews.country = reviews.country.fillna('Unknown')
 
 # replace values
-reviews.taster_twitter_handle = reviews.taster_twitter_handle.replace("@kerinokeefe", "@kerino")
+reviews.taster_twitter_handle = reviews.taster_twitter_handle.replace(
+    "@kerinokeefe", "@kerino")
 
 
 # What are most common wine-producing regions?
@@ -389,16 +384,11 @@ reviews.region_1 = reviews.region_1.fillna('Unknown')
 reviews_per_region = reviews.region_1.value_counts().sort_values(ascending=False)
 
 # or just do it all in 1 line:
-reviews_per_region = reviews.region_1.fillna('Unknown').value_counts().sort_values(ascending=False)
+reviews_per_region = reviews.region_1.fillna(
+    'Unknown').value_counts().sort_values(ascending=False)
 # or this
-reviews_per_region = reviews.fillna({'region_1': 'Unknown'}).groupby('region_1')['region_1'].count().sort_values(ascending=False)
-
-
-
-
-
-
-
+reviews_per_region = reviews.fillna({'region_1': 'Unknown'}).groupby(
+    'region_1')['region_1'].count().sort_values(ascending=False)
 
 
 ######## Renaming & Combining #######
@@ -406,14 +396,14 @@ reviews_per_region = reviews.fillna({'region_1': 'Unknown'}).groupby('region_1')
 # change index/column names - rename()
 
 # rename points column to score:
-reviews_with_score = reviews.rename(columns={'points': 'score'}) 
+reviews_with_score = reviews.rename(columns={'points': 'score'})
 
 # change index name of 1st & 2nd entry
 reviews.rename(index={0: 'firstEntry', 1: 'secondEntry'})
-# set_index usually more convenient than rename of index 
+# set_index usually more convenient than rename of index
 
 
-reviews.rename_axis("wines", axis='rows') # adds title for row indexes
+reviews.rename_axis("wines", axis='rows')  # adds title for row indexes
 reviews.rename_axis("wines", axis='rows').rename_axis("fields", axis='columns')
 
 # Combining
@@ -434,27 +424,23 @@ right = canadian_youtube.set_index(['title', 'trending_date'])
 joined = left.join(right, lsuffix='_CAN', rsuffix='_UK')
 
 
-
-
-
-
-
-
-
 ########################################################################################################
 ####### PRACTICE ########
 ########################################################################################################
 
 # 1. Select name of first wine reviewed for each winery in dataset
 reviews.groupby('winery').title.first()
-reviews.groupby('winery').apply(lambda df: df.title.iloc[0], include_groups=False)
-
+reviews.groupby('winery').apply(
+    lambda df: df.title.iloc[0], include_groups=False)
 
 
 # 2. Pick best wine by country and province
 # Most points
-previous_most_points = reviews.groupby(['country', 'province']).apply(lambda df: df.loc[df['points'].idxmax()], include_groups=False).points.sort_values(ascending=False)
-my_solution_most_points = reviews.groupby(['country', 'province'])['points'].agg('max').sort_values(ascending=False)
+previous_most_points = reviews.groupby(['country', 'province']).apply(
+    lambda df: df.loc[df['points'].idxmax()], include_groups=False).points.sort_values(ascending=False)
+
+my_solution_most_points = reviews.groupby(['country', 'province'])[
+    'points'].agg('max').sort_values(ascending=False)
 
 
 # 3. Pick best wine by country and province for the value
@@ -465,7 +451,8 @@ reviews['ratio'] = reviews['points'] / reviews['price']
 
 # Step 2: Use the ratio column within the groupby operation and calculate the index of the max ratio
 
-best_vals = reviews.loc[reviews.groupby(['country', 'province'])['ratio'].idxmax().sort_values(ascending=False)]
+best_vals = reviews.loc[reviews.groupby(['country', 'province'])[
+    'ratio'].idxmax().sort_values(ascending=False)]
 # print(reviews.loc[reviews.title == 'Nicosia 2013 Vulkà Bianco (Etna)'])
 
 
@@ -477,10 +464,12 @@ best_rating_per_price = reviews.groupby('price')['points'].max().sort_values()
 
 
 # 6. min and max prices for each variety of wine
-min_max_prices = reviews.groupby('variety')['price'].agg(['min', 'max']).sort_values(by=['min', 'max'])
+min_max_prices = reviews.groupby('variety')['price'].agg(
+    ['min', 'max']).sort_values(by=['min', 'max'])
 
 # 7. Most expensive wine varieties
-most_exp_varieties = reviews.groupby('variety')['price'].max().sort_values(ascending=False)
+most_exp_varieties = reviews.groupby(
+    'variety')['price'].max().sort_values(ascending=False)
 
 # 8. Average scores by reviewers
 reviews.groupby('taster_twitter_handle')['points'].mean()
@@ -492,28 +481,31 @@ reviews.groupby(['country', 'variety']).size().sort_values(ascending=False)
 ### A few more practice prompts ###
 
 # 10. Find the winery with the most reviews in each country
-most_reviewed_winery_per_country = reviews.groupby(['country', 'winery']).size().sort_values(ascending=False)
+most_reviewed_winery_per_country = reviews.groupby(
+    ['country', 'winery']).size().sort_values(ascending=False)
 
 # 11. Get the average price per variety and province
-reviews.groupby(['variety', 'province']).price.agg('mean').sort_values(ascending=False)
+reviews.groupby(['variety', 'province']).price.agg(
+    'mean').sort_values(ascending=False)
 
 # 12. Group by 'taster_twitter_handle', calculate the mean points, and get the top 5 tasters
-reviews.groupby('taster_twitter_handle')['points'].mean().sort_values(ascending=False).head(5)
+reviews.groupby('taster_twitter_handle')[
+    'points'].mean().sort_values(ascending=False).head(5)
 
 # 13. Group by 'winery', find the wine with the highest price for each winery
 # print('mine:')
-highest_price_per_winery = reviews.groupby('winery')['price'].max().sort_values(ascending=False)
+highest_price_per_winery = reviews.groupby(
+    'winery')['price'].max().sort_values(ascending=False)
 # print(highest_price_per_winery)
-ais_highest_price_per_winery = reviews.loc[reviews.groupby('winery')['price'].idxmax()].sort_values(by=['price'], ascending=False)
+ais_highest_price_per_winery = reviews.loc[reviews.groupby(
+    'winery')['price'].idxmax()].sort_values(by=['price'], ascending=False)
 
 # 14. Group by 'country' 'variety', count the total number of reviews
-total_reviews_per_country_variety = reviews.groupby(['country', 'variety']).size().reset_index(name='the total')
-
-
+total_reviews_per_country_variety = reviews.groupby(
+    ['country', 'variety']).size().reset_index(name='the total')
 
 
 ##### A bit harder #####
-
 
 
 # 15. Find the Average Score of Wines for Each Taster, Ignoring Any Tasters Who Have Fewer Than 2 Reviews:
@@ -521,7 +513,8 @@ total_reviews_per_country_variety = reviews.groupby(['country', 'variety']).size
 taster_review_counts = reviews.groupby('taster_twitter_handle').size()
 
 # Filter out tasters with fewer than 2 reviews
-frequent_tasters = taster_review_counts[taster_review_counts >= 2].index # get list of taster_twitter_handles with more than 2 reviews
+# get list of taster_twitter_handles with more than 2 reviews
+frequent_tasters = taster_review_counts[taster_review_counts >= 2].index
 
 # Calculate the average score for each taster with at least 2 reviews
 # build df from ones that have the frequent tasters as their taster_twitter_handle
@@ -537,12 +530,13 @@ avg_scores_frequent_tasters = (
 
 # 16. # Identify the Winery with the Highest Average Points per Country:
 # # Group by 'country' and 'winery', calculate the mean points, and find the winery with the highest average points in each country
-highest_avg_points_winery_per_country = ( 
+highest_avg_points_winery_per_country = (
     reviews.groupby(['country', 'winery'])
-        .points
-        .mean()  # Calculate the average points for each country-winery combination
-        .groupby(level=0)  # Re-group by just the country (the mean calculation created a multi-index of (country, winery))
-        .idxmax()  # Get the indexes (country, winery) with the highest average points within each country
+    .points
+    .mean()  # Calculate the average points for each country-winery combination
+    # Re-group by just the country (the mean calculation created a multi-index of (country, winery))
+    .groupby(level=0)
+    .idxmax()  # Get the indexes (country, winery) with the highest average points within each country
 )
 
 
